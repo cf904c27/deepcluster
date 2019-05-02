aef095114f1f101f1c4e950df9784ccee145b45b
cf904c27 <34496078+cf904c27@users.noreply.github.com>
2019-05-02 10:55:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -293,7 +293,7 @@ def compute_features(dataloader, model, N):
     model.eval()
     # discard the label information in the dataloader
     for i, (input_tensor, _) in enumerate(dataloader):
-        input_var = torch.autograd.Variable(input_tensor.cuda(), volatile=True)
+        input_var = torch.autograd.Variable(input_tensor.cuda()) #, volatile=True)
         aux = model(input_var).data.cpu().numpy()
 
         if i == 0:
